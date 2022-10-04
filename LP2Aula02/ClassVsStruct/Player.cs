fcf44dd811cea1543ef5c5cdb1291727ba0eb086
Pedro Coutinho <pedro_coutinho18@outlook.com>
2022-10-04 10:17:26 +0100

    Add ClassVsStructs

---
+++ LP2Aula02/ClassVsStruct/Player.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassVsStruct
+{
+    public struct Player
+    {
+        public int Health {get; set;}
+
+        public int Armor {get; set;}
+    }
+}
---
+++ LP2Aula02/ClassVsStruct/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClassVsStruct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player1 = new Player(){Health = 90, Armor = 20};
+            Player player2 = player1;
+
+            Console.WriteLine($"Player1 Health:{player1.Health} Player1 Armor:{player1.Armor}");
+            Console.WriteLine($"Player2 Health:{player2.Health} Player2 Armor:{player2.Armor}");
+
+            Console.WriteLine("-----------");
+
+            player1.Health = 11;
+            player1.Armor = 20;
+
+            Console.WriteLine($"Player1 Health:{player1.Health} Player1 Armor:{player1.Armor}");
+            Console.WriteLine($"Player2 Health:{player2.Health} Player2 Armor:{player2.Armor}");
+
+        }
+    }
+}
