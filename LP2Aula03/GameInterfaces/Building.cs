fefb15fa3f9b42515325e75c79a8b9534744e9d7
Pedro Coutinho <pedro_coutinho18@outlook.com>
2022-10-11 10:58:39 +0100

    Add Exercício 3

---
+++ LP2Aula03/GameInterfaces/Building.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameInterfaces
+{
+    public class Building : IHasValue
+    {
+        public string Tipo {get; }
+
+        public float Value {get; }
+
+        public float Area {get;}
+
+        public Building(string tipo, float value, float area)
+        {
+            Tipo = tipo;
+
+            Value = value;
+
+            Area = area;
+
+        }
+
+        public bool Equals (IHasValue other)
+        {
+            if (other == null)
+                return false;
+            
+            if (this.Value == other.Value)
+                return true;
+            else
+                return false;
+        }
+
+        public override string ToString() =>
+            $"{Tipo,-20}{Value,8:f2}${Area,8:f2}m2";
+    
+    }
+}
---
+++ LP2Aula03/GameInterfaces/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GameInterfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IHasValue[] unitsAndBuildings = new IHasValue[5];
+            unitsAndBuildings[0] = new Building("Casa",10, 30);
+            unitsAndBuildings[1] = new SettlerUnit(2, 10);
+            unitsAndBuildings[2] = new MilitaryUnit(7, 20, 1);
+            unitsAndBuildings[3] = new SettlerUnit(4, 7);
+            unitsAndBuildings[4] = new Building("Prédio",100, 20);
+
+            foreach (IHasValue something in unitsAndBuildings)
+            {
+                Console.WriteLine(something);
+            }
+        }
+    }
+}
