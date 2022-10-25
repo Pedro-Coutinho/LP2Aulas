72886907047f35d324448410d36c7e18be885524
Pedro Coutinho <pedro_coutinho18@outlook.com>
2022-10-25 10:44:53 +0100

    Add ExceptionTest

---
+++ LP2Aula05/ExceptionTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExceptionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insere um numero inteiro: ");
+            int i = 0;
+            try
+            {
+                i = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Numero inserido : {i}");
+
+            } catch (Exception){
+                Console.WriteLine("Input não é um int");
+            }
+        }
+    }
+}
