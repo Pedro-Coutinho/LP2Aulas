690cbbc875521d80750ae3283077c083a3fa49bc
Pedro Coutinho <pedro_coutinho18@outlook.com>
2022-11-29 12:18:57 +0000

    Add LINQFluent

---
+++ LP2Aula08/LINQFluent/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+
+namespace LINQFluent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] myfile = File.ReadAllLines("roger1.txt");
+
+            int numOFlines = myfile.Count(s => s.Length > 30);
+            double average = myfile.Average(s => s.Length);
+            bool biggerThan120 = myfile.Any(s => s.Length > 120);
+            IEnumerable<string> stringIEnum = myfile
+                .Where(s => s.Contains('Y'))
+                .Select(s => s.Trim().Split()[0]?.ToUpper());
+            
+            
+            Console.WriteLine(numOFlines);
+            Console.WriteLine(average);
+            Console.WriteLine(biggerThan120);
+            
+            foreach(string s in stringIEnum)
+                Console.WriteLine(s);
+
+
+        }
+    }
+}
