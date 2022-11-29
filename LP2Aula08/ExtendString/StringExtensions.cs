22d66b06d21458ab508c056c9cc24efb02468950
Pedro Coutinho <pedro_coutinho18@outlook.com>
2022-11-29 09:53:30 +0000

    Add StringExtensions

---
+++ LP2Aula08/ExtendString/StringExtensions.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace ExtendString
+{
+    public static class StringExtensions
+    {
+        private static Random random;
+
+        static StringExtensions()
+        {
+            random = new Random();
+        }
+        public static string ToRandomCase(this string s)
+        {
+            
+            StringBuilder newS = new StringBuilder();
+
+            foreach(char c in s)
+            {
+
+                double rndN = random.NextDouble();
+                if(rndN <= 0.5)
+                {
+                    newS.Append(char.ToUpper(c));
+                }
+                else
+                {
+                    newS.Append(char.ToLower(c));
+                }   
+            }
+            return newS.ToString();
+            
+        }
+        
+    }
+}
